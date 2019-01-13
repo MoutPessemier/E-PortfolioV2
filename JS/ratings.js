e137303e2aaa30249c9e36bd2ccd5136dd3adabd
Mout Pessemier <moutpessemier@hotmail.com>
2019-01-13 23:07:07 +0100

    front image fix, ChezMout start

---
+++ JS/ratings.js
@@ -0,0 +1,14 @@
+let feebackList = this.viewmodel;
+
+function init() {
+    let outerdiv = document.getElementById("ratings");
+    feedbackList.foreach(el => {
+
+
+
+
+    });
+
+}
+
+window.onload = () => init();
