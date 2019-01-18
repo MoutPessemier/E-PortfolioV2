5103290e843ea2ddfc4bcd9e2d54e4e1b4979c5b
Mout Pessemier <moutpessemier@hotmail.com>
2019-01-18 13:30:17 +0100

    Final commit V1 Frontend completely done at this point Backend will be constructed in next version

--- JS/werktniet.js
+++ JS/werktniet.js
@@ -1,9 +1,11 @@
 let alertM;
 
 if (document.documentElement.lang == "en") {
-    alertM = "This is under construction, doesn't work yet."
+    alertM = `This is under construction, doesn't work yet. Once the next version is up and running, you'll be able to send me as many emails as you like!
+    The new version is under construction at the moment.`
 } else if (document.documentElement.lang == "nl") {
-    alertM = "Deze functie is nog onder constructie en werkt nog niet."
+    alertM = `Deze functie is nog onder constructie en werkt nog niet. Van zodra de volgende versie online komt, kun je mij zo veel mails sturen als je maar wil
+    De nieuwe versie wordt gemaakt op dit moment.`
 }
 
 function init() {
